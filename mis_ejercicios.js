c983180cc0b253a7f1924294f43f486ba652a6e0
JotaJotaM1 <johnjames_89@hotmail.com>
2022-03-30 00:33:35 -0500

    identacion

--- mis_ejercicios.js
+++ mis_ejercicios.js
@@ -2,66 +2,60 @@ const prompt = require("prompt-sync")({ sigint: true });
 
 // 1) Convertidor de centimetros a pulgadas:
 
-let conversion = (numero) =>{
+let conversion = (numero) => {
+  let pulgadas = 2.54;
 
-    let pulgadas = 2.54
+  let centimetros = pulgadas * numero;
 
-let centimetros = pulgadas * numero
+  return `${centimetros} ${"Este es el resultado en pulgadas"}`;
+};
 
-return `${centimetros} ${"Este es el resultado en pulgadas"}`
-}
+let numCm = prompt("Dame una medida en centimentros ");
 
-let numCm = prompt("Dame una medida en centimentros ")
-
-console.log(conversion(numCm))
+console.log(conversion(numCm));
 
 // 2) Concatenacion:
 
 let convertidorUrl = (url) => {
-
-    url = "pepito"
-         return `http://www.${url}.com.co` 
- }
-console.log(convertidorUrl())
+  url = "pepito";
+  return `http://www.${url}.com.co`;
+};
+console.log(convertidorUrl());
 
 // 3) Frase con string mas !
 
-let fraseConAdmiracion = (frase) =>{
+let fraseConAdmiracion = (frase) => {
+  return `${"!"} ${frase.toUpperCase()} ${"¡"}`;
+};
 
-     return `${"!"} ${frase.toUpperCase()} ${"¡"}`
- }
+let frase = prompt("Dame una frase ");
 
- let frase = prompt("Dame una frase ")
-
- console.log((fraseConAdmiracion(frase)))
+console.log(fraseConAdmiracion(frase));
 
 // // 4) Crear una función que calcule la edad de los perros, considerando que 1 año
 // para nosotros son 7 de ellos.
 
-let CalEdadPerro= (numero) =>{
+let CalEdadPerro = (numero) => {
+  let edadPerro = numero * 7;
 
- let edadPerro = numero * 7
+  return `${edadPerro} ${"Años equivalen a su vida perruna"}`;
+};
+let añosHumanos = prompt("Dame la edad de tu perro ");
 
-return `${edadPerro} ${"Años equivalen a su vida perruna"}`
-
-}
-let añosHumanos = prompt("Dame la edad de tu perro ")
-
-console.log(CalEdadPerro(añosHumanos))
+console.log(CalEdadPerro(añosHumanos));
 
 // 5) Crear una función que calcule el valor de tu hora de trabajo, introduciendo tu
 // sueldo mensual como parámetro.
 // PD: considerá que tu mes de trabajo tiene 192 horas.
 
-let CalHoraTrabajo = (mensual) =>{
-    
-    let salarioMensual = mensual / prompt("Cuantas horas trabajas al mes ")
+let CalHoraTrabajo = (mensual) => {
+  let salarioMensual = mensual / prompt("Cuantas horas trabajas al mes ");
 
-    let horaValor = salarioMensual
+  let horaValor = salarioMensual;
 
-    return `${"Tu hora de trabajo cuesta "} ${horaValor}`
-}
+  return `${"Tu hora de trabajo cuesta "} ${horaValor}`;
+};
 
-let Salario = prompt("Cuanto ganas al mes ? ")
+let Salario = prompt("Cuanto ganas al mes ? ");
 
-console.log(CalHoraTrabajo(Salario))
+console.log(CalHoraTrabajo(Salario));
